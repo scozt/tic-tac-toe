31c228a2d1497d40dfa602114c65c06887f3e560
scozt <mr.oeztuerk@gmail.com>
2020-07-09 12:51:54 +0200

    add field divs and field style

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,10 @@
 const containerDiv = document.createElement("div");
 containerDiv.id = "container-grid";
+
+for(let index = 0; index < 9; index++) {
+    const fieldDiv = document.createElement('div');
+    fieldDiv.className = 'field';
+    fieldDiv.id = index;
+    containerDiv.appendChild(fieldDiv);
+}
 document.body.appendChild(containerDiv);
